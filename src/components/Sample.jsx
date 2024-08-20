bd5b0443dff5c58d996343307b4c737c17c24955
rehan700 <rehansource3@gmail.com>
2024-08-20 21:04:15 +0530

    21:05

--- src/components/Sample.jsx
+++ src/components/Sample.jsx
@@ -13,7 +13,7 @@ export default function Sample() {
   useEffect(() => {
     const fetchWeather = () => {
       const proxyUrl = "https://cors-anywhere.herokuapp.com/";
-      fetch(`http://api.weatherapi.com/v1/current.json?key=2ec1f9dfbc0f4e77b1953647241908&q=${cityText}&aqi=no`)
+      fetch(`https://api.weatherapi.com/v1/current.json?key=2ec1f9dfbc0f4e77b1953647241908&q=${cityText}&aqi=no`)
         .then(response => {
           if (!response.ok) {
             throw new Error('Network response was not ok');
@@ -48,7 +48,7 @@ export default function Sample() {
   const handleClick = () => {
     const cname = cityName || 'Mumbai';
     const proxyUrl = "https://cors-anywhere.herokuapp.com/";
-    let url = `http://api.weatherapi.com/v1/current.json?key=2ec1f9dfbc0f4e77b1953647241908&q=${cityText}&aqi=no`;
+    let url = `https://api.weatherapi.com/v1/current.json?key=2ec1f9dfbc0f4e77b1953647241908&q=${cityText}&aqi=no`;
     fetch(url)
       .then(response => {
         if (!response.ok) {
